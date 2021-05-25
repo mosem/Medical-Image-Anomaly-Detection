586d6ecf5c2c9cea182e44c782803552b08a960e
mosem <mandelmoshe@gmail.com>
2021-05-25 16:52:52 +0300

    fixed 3DRsnaConcatDataset

--- Panda/rsnaDataset.py
+++ Panda/rsnaDataset.py
@@ -35,9 +35,7 @@ def normalize_dicom(dicom):
 
     image_array = np.dstack([soft_tissue,subdural,brain])
     image_array = (image_array*255).astype(np.uint8)
-    image = Image.fromarray(image_array)
-    return image
-
+    return image_array
 
 class RsnaDataset3D(torch.utils.data.Dataset):
 
@@ -101,7 +99,7 @@ class RsnaDataset(torch.utils.data.Dataset):
         if (type(img_label) != int):
             img_label = img_label.item()
         dicom_image = dicom.dcmread(img_path)
-        normalized_image = normalize_dicom(dicom_image)
+        normalized_image = Image.fromarray(normalize_dicom(dicom_image))
         tensor_image = self.transform(normalized_image)
         return tensor_image, img_label
 
