80ba940f2f676d074db1ac674509274db3f0294f
mosem <mandelmoshe@gmail.com>
2021-05-26 13:02:26 +0300

    added timesformer library

--- Panda/TimeSformerUtils.py
+++ Panda/TimeSformerUtils.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
+from timesformer.models.vit import TimeSformer
 
 model_urls = {
     'timesformer-HR': 'https://www.dropbox.com/s/9t68uzk8w2fpfnv/TimeSformer_divST_16_448_SSv2.pyth?dl=0'
 }
 
-class TimeSformer(nn.Module):
+class TimeSformerWrapper(nn.Module):
 
     def __init__(self, model_url=model_urls['timesformer-HR']):
         self.timesformer_model = TimeSformer(img_size=448, num_classes=400, num_frames=16, attention_type='divided_space_time',
