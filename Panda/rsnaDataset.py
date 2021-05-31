c51a786847a7bfa0c56a7489a186acff3a97a2a2
mosem <mandelmoshe@gmail.com>
2021-05-31 14:23:23 +0300

    getting timesformer to work - read png directly

--- Panda/rsnaDataset.py
+++ Panda/rsnaDataset.py
@@ -63,8 +63,8 @@ class RsnaDataset3D(torch.utils.data.Dataset):
             # dicom_image = dicom.dcmread(img_path)
             # normalized_image = normalize_dicom(dicom_image)
             # tensor_image = self.transform(normalized_image) # CxHxW
-            tensor_image = self.transform(image) # CxHxW
-            images.append(tensor_image)
+                tensor_image = self.transform(image) # CxHxW
+                images.append(tensor_image)
         return torch.stack(images, dim=1) # CxFxHxW
 
 
