55683a3f72d081ca19cb5254f65f0a39af932a1a
mosem <mandelmoshe@gmail.com>
2021-06-08 15:18:48 +0300

    dealing with 3d data when 2d rsna is used - referencing inner layers

--- Panda/ResNet.py
+++ Panda/ResNet.py
@@ -273,6 +273,7 @@ class ResNet3D(nn.Module):
         output_pred = [] # batch_size x frames x 1
         output_features = [] # batch_size x frames x features_dimension
         for sample in torch.split(x, 1, 0):
+            sample = sample.squeeze()
             # sample is size of channels x frames x height x width
             print(sample.size())
             slices = sample.permute((1,0,2,3)) # frames x channels x height x width
